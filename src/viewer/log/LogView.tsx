e2465d71b92ca741cfd372aae7975bb164ac5286
Sven Weiss <sven.weiss@tno.nl>
2023-04-18 13:38:11 +0200

    Usability: removed the checkbox from header entries

--- src/viewer/log/LogView.tsx
+++ src/viewer/log/LogView.tsx
@@ -152,7 +152,6 @@ export default class LogView extends React.Component<Props, State> {
             <ReactResizeDetector handleWidth key={index} onResize={(width)=>this.setColumnWidth(value, width!)}>
             <div className="resizable-content" style={style} key={index}>
                 <div style={innerStyle}>
-                <input type="checkbox" id="showMini" key={index} checked onChange={(e) => this.handleCheckbox(e, index)}></input>
                     {value}
                 </div>
             </div>
