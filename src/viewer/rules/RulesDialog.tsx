5475f275d40128db97df430c030502fd89b4b7d7
ckitsanelis <ckitsanelis@gmail.com>
2023-04-21 13:21:19 +0200

    Additional options to define complex state based rules

--- src/viewer/rules/RulesDialog.tsx
+++ src/viewer/rules/RulesDialog.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Rule from './Rule';
-import { VSCodeButton, VSCodeDivider, VSCodeDropdown, VSCodeOption, VSCodeTextField } from '@vscode/webview-ui-toolkit/react';
-import StateBasedRule from './StateBasedRule';
-import Table from './Table';
 import LogFile from '../LogFile';
+import StateBasedRule from './StateBasedRule';
+import Table from './Tables/Table';
+import { VSCodeButton, VSCodeTextField, VSCodeDropdown, VSCodeOption } from '@vscode/webview-ui-toolkit/react';
+
 
 interface Props {
     onClose: (rules: Rule[]) => void;
@@ -12,8 +13,9 @@ interface Props {
 }
 
 interface State {
-    selectedRule: undefined | number;
+    showEdit: boolean;
     rules: Rule[];
+    selectedRule: number;
 }
 
 const BACKDROP_STYLE: React.CSSProperties = {
@@ -26,34 +28,7 @@ const DIALOG_STYLE: React.CSSProperties = {height: '95%', width: '95%', padding:
 export default class RulesDialog extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {selectedRule: undefined, rules: props.initialRules}
-    }
-
-    renderManage() {
-        const onAddAction = () => {
-            const newRule = new StateBasedRule(`Rule${this.state.rules.length + 1}`, '', '', []);
-            this.setState({rules: [...this.state.rules, newRule], selectedRule: this.state.rules.length});
-        }
-
-        const onDeleteAction = (index: number) => {
-            if (this.state.selectedRule === index) this.setState({selectedRule: undefined});
-            this.setState({rules: this.state.rules.filter((r, i) => index !== i)});
-        }
-
-        return (
-            <div style={{marginTop: '10px'}}>
-                <Table
-                    title='Manage rules'
-                    columns={[{name: 'Column', width: '15%'}, {name: 'Type', width: '15%'}, {name: 'Description', width: ''}]}
-                    rows={this.state.rules.map((rule) => [rule.column, rule.friendlyType, rule.description])} 
-                    noRowsText={'No rules defined yet (press + to add)'}
-                    onAddAction={onAddAction}
-                    onEditAction={(selectedRule) => this.setState({selectedRule})}
-                    onDeleteAction={onDeleteAction}
-                    highlightRow={this.state.selectedRule}
-                />
-            </div>
-        )
+        this.state = {showEdit: false, selectedRule: -1, rules: props.initialRules}
     }
 
     updateRule(rule: Rule, index: number) {
@@ -62,27 +37,56 @@ export default class RulesDialog extends React.Component<Props, State> {
         this.setState({rules});
     }
 
+    renderManage() {
+        const onAddAction = () => {
+            const newRule = new StateBasedRule(`Rule${this.state.rules.length + 1}`, '', 0, 0, 0, []);
+            this.setState({rules: [...this.state.rules, newRule], selectedRule: this.state.rules.length, showEdit: true});
+        }
+
+        const onEditAction = (index: number) => {
+            this.setState({showEdit: true, selectedRule: index});
+        }
+
+        const onDeleteAction = (index: number) => {
+            if (this.state.selectedRule === index) this.setState({selectedRule: -1});
+            this.setState({rules: this.state.rules.filter((r, i) => index !== i)});
+        }
+    
+
+        return (
+            <div style={{marginTop: '10px'}}>
+                <Table
+                    // title='Manage rules'
+                    columns={[{name: 'Name', width: '150px'}, {name: 'Type', width: '150px'}, {name: 'Description', width: ''}]}
+                    rows={this.state.rules.map((rule) => [rule.column, rule.friendlyType, rule.description])} 
+                    noRowsText={'No rules have been defined (click + to add)'}
+                    onAddAction={onAddAction}
+                    onEditAction={onEditAction}
+                    onDeleteAction={onDeleteAction}
+                    highlightRow={this.state.selectedRule}
+                />
+            </div>
+        )
+    }
+
+
+
     renderEdit() {
-        if (this.state.selectedRule === undefined) return;
-        const rule = this.state.rules[this.state.selectedRule];
+        if (this.state.selectedRule === -1) return;
         const ruleIndex = this.state.selectedRule;
+        const rule = this.state.rules[ruleIndex];
+        const user_columns = this.state.rules.map((r, i) => r.column)
         const defaultRuleColumn = `Rule${ruleIndex + 1}`;
         const typeOptions = [StateBasedRule];
         const keyWidth = '100px';
         const textFieldWidth = '250px';
         const rows = [
             [
-                ('Column'),
+                ('Name'),
                 <VSCodeTextField 
                     style={{width: textFieldWidth}}
                     initialValue={rule.column} 
                     onInput={(e) => this.updateRule(rule.setColumn(e.target.value || defaultRuleColumn), ruleIndex)}/>
-            ],
-            [
-                ('Type'),
-                <VSCodeDropdown style={{width: textFieldWidth, marginBottom: '2px'}} value={typeOptions.findIndex(o => rule instanceof o).toString()} onChange={() => 'TODO'}>
-                    {typeOptions.map((o, i) => <VSCodeOption key={i} value={i.toString()}>{o.friendlyType}</VSCodeOption>)}
-                </VSCodeDropdown>
             ],
             [
                 ('Description'),
@@ -91,35 +95,57 @@ export default class RulesDialog extends React.Component<Props, State> {
                     initialValue={rule.description} 
                     onInput={(e) => this.updateRule(rule.setDescription(e.target.value), ruleIndex)}/>
             ],
+            [
+                ('Type'),
+                <VSCodeDropdown disabled style={{width: textFieldWidth, marginBottom: '2px'}} value={typeOptions.findIndex(o => rule instanceof o).toString()} onChange={() => 'TODO'}>
+                    {typeOptions.map((o, i) => <VSCodeOption key={i} value={i.toString()}>{o.friendlyType}</VSCodeOption>)}
+                </VSCodeDropdown>
+            ],
         ];
 
         return (
-            <div>
+            <div style={{marginTop: '25px'}}>
                 <Table
-                    title='Edit rule'
+                    // title='Edit rule'
                     hideHeader={true}
                     rows={rows}
                     columns={[{width: '100px'}, {width: ''}]}
                 />
-                {rule.renderEdit((newRule) => this.updateRule(newRule, ruleIndex), keyWidth, textFieldWidth, this.props.logFile)}
+                {rule.renderEdit((newRule) => this.updateRule(newRule, ruleIndex), keyWidth, textFieldWidth, user_columns, this.props.logFile)}
             </div>
         )
     }
 
     render() {
+        const onReturn = () => {
+            const ruleIndex = this.state.selectedRule;
+            const rule = this.state.rules[ruleIndex];
+            this.updateRule(rule.reset(), ruleIndex)
+            this.setState({showEdit: false});
+        }
+
         return (
             <div style={BACKDROP_STYLE}>
                 <div className='dialog' style={DIALOG_STYLE}>
                     <div style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'top'}}>
-                        <div className='title-big'>Rules</div>
-                        <VSCodeButton appearance='icon' onClick={() => this.props.onClose(this.state.rules)}>
-                            <i className='codicon codicon-close'/>
-                        </VSCodeButton>
+                        {   !this.state.showEdit &&
+                            <div className='title-big'>Annotation Columns</div>
+                        }
+                        {   this.state.showEdit &&
+                            <div className='title-big'>Edit State-Based Annotation Column</div>
+                        }
+                        {   this.state.showEdit &&
+                            <VSCodeButton style={{marginLeft: 'auto'}} appearance='icon' onClick={onReturn}>
+                                <i className='codicon codicon-arrow-left'/>
+                            </VSCodeButton>
+                        }
+                            <VSCodeButton appearance='icon' onClick={() => this.props.onClose(this.state.rules)}>
+                                <i className='codicon codicon-close'/>
+                            </VSCodeButton>
                     </div>
                     <div style={{display: 'flex', flexDirection: 'column', flex: 1}}>
-                        {this.renderManage()}
-                        <VSCodeDivider style={{marginTop: '20px', marginBottom: '20px'}}/>
-                        {this.renderEdit()}
+                        {!this.state.showEdit && this.renderManage()}
+                        {this.state.showEdit && this.renderEdit() }
                     </div>
                 </div>
             </div>
---
+++ src/viewer/rules/Tables/StateTable.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
+
+interface Props {
+    title?: string;
+    columns: {name?: string, width: string}[];
+    rows: (JSX.Element | string)[][];
+    start_state: number;
+    hideHeader?: boolean
+    noRowsText?: string;
+    onAddAction?: () => void;
+    highlightRow?: number | null;
+    onDeleteAction?: (index: number) => void;
+    onSetAction?: (index: number) => void;
+}
+
+const ACTIONS_WIDTH = '50px';
+
+export default class StateTable extends React.Component<Props> {
+    render() {
+        return (
+            <div style={{width: '50%'}}>
+                {this.props.title && <div className='title-small'>{this.props.title}</div>}
+                <table style={{width: '250px', borderSpacing: 0}}>
+                    <tbody>
+                    {!this.props.hideHeader &&
+                        <tr>
+                            {this.props.columns.map((c, i) => (
+                                <th key={i} style={{textAlign: 'left', width: c.width, fontWeight: 200}}>{c.name ?? ''}</th>
+                            ))}
+                            {(this.props.onDeleteAction || this.props.onSetAction) && <th style={{textAlign: 'left', width: ACTIONS_WIDTH, fontWeight: 200}}>Initial</th>}
+                        </tr>
+                    }
+                    {this.props.rows.map((row, i) => {
+                        return (
+                            <tr key={i} className={i === this.props.highlightRow ? 'list-hover-background' : ''}>
+                                {row.map((r, ii) => <td key={ii} width={this.props.columns[ii].width}>{r}</td>)}
+                                {(this.props.onDeleteAction || this.props.onSetAction) && (
+                                    <td width={ACTIONS_WIDTH}>
+                                        <div style={{display: 'flex', justifyContent: (this.props.onDeleteAction && this.props.onSetAction) ? 'space-between' : 'center'}}>
+                                            { this.props.onSetAction && this.props.start_state === i &&
+                                                <VSCodeButton appearance='icon' onClick={() => this.props.onSetAction?.(i)}>
+                                                    <i className='codicon codicon-record'/>
+                                                </VSCodeButton>
+                                            }
+                                            { this.props.onSetAction && this.props.start_state !== i &&
+                                                <VSCodeButton appearance='icon' onClick={() => this.props.onSetAction?.(i)}>
+                                                    <i className='codicon codicon-circle-filled'/>
+                                                </VSCodeButton>
+                                            }
+                                            <div style={{marginLeft: '20px'}}>
+                                            { this.props.onDeleteAction &&
+                                                <VSCodeButton appearance='icon' onClick={() => this.props.onDeleteAction?.(i)}>
+                                                    <i className='codicon codicon-trash'/>
+                                                </VSCodeButton>
+                                            }
+                                            </div>
+                                        </div>
+                                    </td>
+                                )}
+                            </tr>
+                        )
+                    })}
+                    {(this.props.onAddAction && this.props.noRowsText && this.props.rows.length === 0) && (
+                        <tr>
+                            <td colSpan={this.props.columns.length + 1} style={{textAlign: 'center'}}>
+                                <i>{this.props.noRowsText}</i>
+                            </td>
+                        </tr>
+                    )}
+                    {this.props.onAddAction && (
+                        <tr>
+                            <td colSpan={this.props.columns.length + 1} style={{textAlign: 'right'}}>
+                                <VSCodeButton appearance='icon' onClick={() => this.props.onAddAction?.()}>
+                                    <i className='codicon codicon-plus'/>
+                                </VSCodeButton>
+                            </td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
